eb4f8769cdcd1f079ddedca787bcfd1136356bcc
Corey Arnold <carnold@newrelic.com>
2021-06-07 17:39:50 -0700

    add in release metrics for last month and this month

--- metrics.py
+++ metrics.py
@@ -1,7 +1,12 @@
+import calendar
+import datetime
 import requests
 import os
 import json
 from pprint import pprint
+from datetime import date
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
 
 token = os.getenv('GITHUB_TOKEN', '...')
 
@@ -32,7 +37,6 @@ def getTeamMembers(team_slug):
 				memberlist.append(m['login'])
 
 	return memberlist
-
 
 def getBugCounts(repos):
 	openBugCount = 0
@@ -71,9 +75,51 @@ def getExternalPRCounts(repos,team):
 				print(' found pr for a team member: ', pr['user']['login'])
 	print("Open External PRs: ", prcount)
 
+def get_month_day_range(date):
+	first_day = datetime.combine(date.replace(day = 1), datetime.min.time())
+	last_day = datetime.combine(date.replace(day = calendar.monthrange(date.year, date.month)[1]), datetime.max.time())
+	return first_day, last_day
+
+def getReleaseNumbers(repos):
+	#gets the number of releases across all team repos for the current and previous month
+	#GET /repos/{owner}/{repo}/releases
+	lastmonthreleasecount = 0
+	thismonthreleasecount = 0
+	now = date.today()
+	thismonth = get_month_day_range(now)
+	lastmonth = get_month_day_range(now + relativedelta(months=-1))
+
+	for repo in repos:
+		reporeleasecount_lastmonth = 0
+		reporeleasecount_thismonth = 0
+		query_url = f"https://api.github.com/repos/newrelic/{repo}/releases"
+		params = {
+			"state": "open",
+		}
+		headers = {'Authorization': f'token {token}'}
+		r = requests.get(query_url, headers=headers, params=params)
+		#this has all pull requests. need to filter out ones where the author is on the team
+		releases = r.json()
+
+		for release in releases:
+			releasepublishedat = datetime.strptime(release['published_at'], '%Y-%m-%dT%H:%M:%SZ')
+			if(releasepublishedat > lastmonth[0] and releasepublishedat < lastmonth[1]):
+				print("Release last month in ", repo)
+				reporeleasecount_lastmonth += 1
+			elif(releasepublishedat > thismonth[0] and releasepublishedat < thismonth[1]):
+				print("Release this month in ", repo)
+				reporeleasecount_thismonth += 1
+
+		lastmonthreleasecount += reporeleasecount_lastmonth
+		thismonthreleasecount += reporeleasecount_thismonth
+	print("Total Release Count Last Month: ", lastmonthreleasecount)
+	print("Total Release Count This Month: ", thismonthreleasecount)
+	
+
 teams = readConfigJson()
 for team in teams['team']:
 	print(team['name'].upper())
 	getBugCounts(team['repos'])
 	getExternalPRCounts(team['repos'], team['team-slug'])
+	getReleaseNumbers(team['repos'])
 	print()	
